3c36197579c1b8cbf383f13b7ee571d3ce22bf22
GerardoCano <gczzamaichigo@gmail.com>
2020-03-05 19:33:46 -0600

    cx

--- functions/index.js
+++ functions/index.js
@@ -12,20 +12,20 @@ exports.mensaje = functions.https.onRequest(
   (request, response) => {
     try {
       // verifica que el parámetro nombre recibido del navegador esté correcto.
-      if (!request.query.nombre1) {
+      if (!request.query.nombre) {
         // Entra aquí si el nombre es null, undefined o ""
-        throw new Error("Falta el nombre 1");
-      } else if (!request.query.nombre2) {
+        throw new Error("Falta el nombre");
+      } else if (!request.query.pasatiempo) {
         // Entra aquí si el pasatiempo es null, undefined o ""
         throw new Error("Falta el pasatiempo");
-      }else if (!request.query.nombre2) {
+      }else if (!request.query.edad) {
         // Entra aquí si el edad es null, undefined o ""
         throw new Error("Falta la edad");
       }
       /* Solo se llega a esta parte si nombre1 y nombre 2 tienen un texto.
        * Devuelve un mensaje. */
       response.send(
-        `A ${request.query.nom} que tiene ${request.query.edad} y nacio entre (${(request.query.edad)-2020} y ${(request.query.edad)-2019}) le gusta ${request.query.pasatiempo}`);
+        `A ${request.query.nombre} que tiene ${request.query.edad} y nacio entre (${2020-(request.query.edad)} y ${2019-(request.query.edad)}) le gusta ${request.query.pasatiempo}`);
     } catch (e) {
       // Devuelve un texto de error.
       response.send(e.message);
